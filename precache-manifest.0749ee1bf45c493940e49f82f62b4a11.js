c594cdffd3c95aaa5acc13987e1c4577de8fa610
Kyle Schoch <kgschoch@gmail.com>
2018-10-24 15:50:04 -0700

    Updates

---
+++ precache-manifest.0749ee1bf45c493940e49f82f62b4a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "a51cd1768a8da6dfd674",
+    "url": "/clicky-game/static/js/main.a51cd176.chunk.js"
+  },
+  {
+    "revision": "7ccad06e8a96e8ae6253",
+    "url": "/clicky-game/static/js/1.7ccad06e.chunk.js"
+  },
+  {
+    "revision": "a51cd1768a8da6dfd674",
+    "url": "/clicky-game/static/css/main.1f7ed6e0.chunk.css"
+  },
+  {
+    "revision": "d358381b336172fdcee2e32df818202e",
+    "url": "/clicky-game/index.html"
+  }
+];
